105dedbce42ada1efe318784e60322c38b8d7065
vrecek <oktawiusz2002@gmail.com>
2021-12-26 23:40:26 +0100

    Added menu UI

---
+++ game.js
@@ -0,0 +1,150 @@
+
+// win zmiana kto 1
+
+
+/**
+ * CONST
+*/
+const fields = document.querySelectorAll('#b-field');
+const winHeader = document.querySelector('.info h1');
+const turnText = document.querySelector('.info h2 span');
+const restartBtn = document.querySelector('.info button');
+const [X_score, O_score] = document.querySelectorAll('.score h4');
+
+const soundBtn = document.querySelector('.sound i');
+const sound = new Audio('./music.mp3');
+
+let xscore = 0, oscore = 0;
+
+const combos = [
+   [0,1,2], [3,4,5], [6,7,8],
+   [0,3,6], [1,4,7], [2,5,8],
+   [0,4,8], [2,4,6]
+]
+/* -------------------------------------------------------- */
+
+/**
+ * RESET EVERY WIN 
+*/
+let move = 1;
+turnText.textContent = "CROSS'S";
+turnText.style.color = 'rgb(0, 190, 0)';
+let winner = null;
+let board = [
+   ['','',''],
+   ['','',''],
+   ['','','']
+]
+/* -------------------------------------------------------- */
+
+
+fields.forEach(item => item.addEventListener('click', clicked));
+function clicked(e){
+   e.target.className = 'disabled';
+
+   const whosMove = move % 2 === 0 ? true : false;
+
+   const plr = whosMove ? 'far fa-circle' : 'fas fa-times'; 
+   const i = document.createElement('i');
+   i.className = plr;
+   e.target.appendChild(i);
+
+   const {col, row} = e.target.dataset;
+   board[row][col] = plr;
+
+   check();
+
+   move++;
+
+   turnText.textContent = whosMove ? "CROSS" : "CIRCLE";
+   turnText.style.color = whosMove ? 'rgb(0, 190, 0)' : 'red';
+}
+
+function check(){
+   const reduced = board.reduce((prev, curr) => prev.concat(curr));
+
+   const moves = {
+      'far fa-circle': [],
+      'fas fa-times': [],
+   }
+
+   reduced.forEach((item,index) => {
+      moves[item] ? moves[item].push(index) : null;
+   })
+   
+   combos.forEach(item => {
+      if(item.every(it => moves['far fa-circle'].indexOf(it) > -1)) winner = 'CIRCLE'
+      if(item.every(it => moves['fas fa-times'].indexOf(it) > -1)) winner = 'CROSS'
+   })
+   
+   if(winner) victory();
+}
+
+
+/**
+ * VICTORY
+*/
+function victory(){
+   const plrWin = winner.includes('CROSS') ? 'fas fa-times' : 'far fa-circle';
+
+   fields.forEach(it => it.className = 'disabled');
+
+   if(plrWin === 'fas fa-times'){
+      xscore++;
+      X_score.textContent = xscore;
+   }else{
+      oscore++;
+      O_score.textContent = oscore;
+   }
+
+   turnText.parentElement.style.display = 'none';
+
+   winHeader.innerHTML = `<i class='${plrWin}'></i> <span>${winner}</span> HAVE WON! <i class='${plrWin}'></i>`;
+   winHeader.style.display = 'block';
+
+   restartBtn.style.display = 'block';
+}
+
+/**
+ * RESTART BUTTON
+*/
+restartBtn.addEventListener('click', () => {
+   move = 1;
+   turnText.textContent = "CROSS";
+   turnText.style.color = 'rgb(0, 190, 0)';
+   winner = null;
+   board = [
+      ['','',''],
+      ['','',''],
+      ['','','']
+   ]
+
+   fields.forEach(it => {
+      it.textContent = '';
+      it.className = '';
+   });
+
+   winHeader.style.display = 'none';
+
+   turnText.parentElement.style.display = 'block';
+
+   restartBtn.style.display = 'none';
+})
+
+/**
+ * SOUND TOGGLE
+*/
+soundBtn.addEventListener('click', () => {
+   if(sound.paused){
+      soundBtn.className = 'fas fa-volume-down';
+      sound.play();
+   }else{
+      soundBtn.className = 'fas fa-volume-mute';
+      sound.pause();
+   }
+})
+
+/**
+ * BACK TO MENU
+*/
+document.querySelector('.home').addEventListener('click', () => window.location.href='./index.html');
--- index.js
+++ index.js
@@ -1,105 +1,33 @@
 /**
- * CONST
+ * START GAME
 */
-const fields = document.querySelectorAll('.board div');
-const winHeader = document.querySelector('.info h1');
-const turnText = document.querySelector('.info h2 span');
-const restartBtn = document.querySelector('.info button');
-const combos = [
-   [0,1,2], [3,4,5], [6,7,8],
-   [0,3,6], [1,4,7], [2,5,8],
-   [0,4,8], [2,4,6]
-]
-/* -------------------------------------------------------- */
+document.querySelector('.play').addEventListener('click', () => window.location.href = './game.html');
 
 /**
- * RESET EVERY WIN 
+ * CLICK CREDITS and OPTIONS
 */
-let move = 1;
-turnText.textContent = "CROSS'S";
-turnText.style.color = 'rgb(0, 190, 0)';
-let winner = null;
-let board = [
-   ['','',''],
-   ['','',''],
-   ['','','']
-]
-/* -------------------------------------------------------- */
+const cont = [document.querySelector('.options'), document.querySelector('.credits')];
+const child = [document.querySelector('.options section'), document.querySelector('.credits section')];
 
-
-fields.forEach(item => item.addEventListener('click', clicked));
-function clicked(e){
-   e.target.className = 'disabled';
-
-   const whosMove = move % 2 === 0 ? true : false;
-
-   const plr = whosMove ? 'far fa-circle' : 'fas fa-times'; 
-   const i = document.createElement('i');
-   i.className = plr;
-   e.target.appendChild(i);
-
-   const {col, row} = e.target.dataset;
-   board[row][col] = plr;
-
-   check();
-
-   move++;
-
-   turnText.textContent = whosMove ? "CROSS'S" : "CIRCLE'S";
-   turnText.style.color = whosMove ? 'rgb(0, 190, 0)' : 'red';
-}
-
-function check(){
-   const reduced = board.reduce((prev, curr) => prev.concat(curr));
-
-   const moves = {
-      'far fa-circle': [],
-      'fas fa-times': [],
-   }
-
-   reduced.forEach((item,index) => {
-      moves[item] ? moves[item].push(index) : null;
+document.querySelectorAll('.menuBtn').forEach((item, ind) => {
+   item.addEventListener('click', () => {
+      child[ind].style.transform = 'translate(-50%,-50%) scale(.2)';
+      setTimeout(() => {
+         child[ind].style.transform = 'translate(-50%,-50%) scale(1)';
+      }, 0);
+      cont[ind].style.display = 'block';
    })
-   
-   combos.forEach(item => {
-      if(item.every(it => moves['far fa-circle'].indexOf(it) > -1)) winner = 'CIRCLE'
-      if(item.every(it => moves['fas fa-times'].indexOf(it) > -1)) winner = 'CROSS'
-   })
-   
-   if(winner) victory();
-}
-
-function victory(){
-   const plrWin = winner.includes('CROSS') ? 'fas fa-times' : 'far fa-circle';
-
-   turnText.parentElement.style.display = 'none';
-
-   winHeader.innerHTML = `<i class='${plrWin}'></i> <span>${winner}</span> HAVE WON! <i class='${plrWin}'></i>`;
-   winHeader.style.display = 'block';
-
-   restartBtn.style.display = 'block';
-}
+})
 
 /**
- * RESTART BUTTON
+ * OPTIONS
 */
-restartBtn.addEventListener('click', () => {
-   move = 1;
-   turnText.textContent = "CROSS'S";
-   turnText.style.color = 'rgb(0, 190, 0)';
-   winner = null;
-   board = [
-      ['','',''],
-      ['','',''],
-      ['','','']
-   ]
 
-   fields.forEach(it => {
-      it.textContent = '';
-      it.className = '';
-   });
-
-   winHeader.style.display = 'none';
-
-   turnText.parentElement.style.display = 'block';
+/**
+ * CLOSE POPUP MENU
+*/
+document.querySelectorAll('.fa-times-circle').forEach(item => {
+   item.addEventListener('click', e => {
+      e.target.parentElement.parentElement.style.display = 'none';
+   })
 })
